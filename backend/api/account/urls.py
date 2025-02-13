a23c5448e9b657edbbdefb2b4edaee8a986ac87b
Eduardo Nogueira <eduardodudunog@gmail.com>
2025-02-13 02:26:03 -0300

    feat(backend): add schedule update, get and deletion endpoints

--- backend/api/account/urls.py
+++ backend/api/account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('schedule', views.schedules)
+    path('schedule', views.schedules),
+    path('schedules/<int:pk>', views.schedule_detail),
 ]
--- backend/api/account/views.py
+++ backend/api/account/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,14 +9,33 @@ from account.models import Schedule
 @api_view(['GET', 'POST'])
 # Create your views here.
 def schedules(request):
-    if (request.method == 'GET'):
+    if request.method == 'GET':
         schedules = Schedule.objects.all()
         serializer = ScheduleSerializer(schedules, many=True)
         return Response(serializer.data)
 
-    elif (request.method == 'POST'):
+    elif request.method == 'POST':
         serializer = ScheduleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT'])
+def schedule_detail(request, pk):
+    schedule = get_object_or_404(Schedule, pk=pk)
+    if request.method == 'GET':
+        serializer = ScheduleSerializer(schedule)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ScheduleSerializer(schedule, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        schedule.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
