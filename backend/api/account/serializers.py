c44d77088bd1afc41fc0c9167aebd7c868bfd7b7
Eduardo Nogueira <eduardodudunog@gmail.com>
2025-02-14 01:49:20 -0300

    refactor(backend): change serializer to save activities of schedule

--- backend/api/account/serializers.py
+++ backend/api/account/serializers.py
@@ -2,12 +2,22 @@ from rest_framework import serializers
 
 from account.models import Schedule, Activity
 
-class ScheduleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Schedule
-        fields = '__all__'
-
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
         fields = '__all__'
+        read_only_fields = ['schedule']
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    activities = ActivitySerializer(many=True)
+
+    class Meta:
+        model = Schedule
+        fields = '__all__'
+
+    def create(self, validated_data):
+        activities_data = validated_data.pop('activities')
+        schedule = Schedule.objects.create(**validated_data)
+        for activity_data in activities_data:
+            Activity.objects.create(schedule=schedule, **activity_data)
+        return schedule
